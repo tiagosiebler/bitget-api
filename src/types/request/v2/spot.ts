661bc55ce1ef632eb4b44bebc390004ef71a7a09
Tim142857 <timothyleroch@gmail.com>
2023-11-16 10:47:20 +0100

    update SpotKlineInterval for V2

--- src/types/request/v2/spot.ts
+++ src/types/request/v2/spot.ts
@@ -1,8 +1,26 @@
-import { SpotKlineInterval } from '../v1/spotV1';
+type SpotKlineIntervalV2 =
+  | '1min'
+  | '5min'
+  | '15min'
+  | '30min'
+  | '1h'
+  | '4h'
+  | '6h'
+  | '12h'
+  | '1day'
+  | '3day'
+  | '1week'
+  | '1M'
+  | '6Hutc'
+  | '12Hutc'
+  | '1Dutc'
+  | '3Dutc'
+  | '1Wutc'
+  | '1Mutc';
 
 export interface SpotCandlesRequestV2 {
   symbol: string;
-  granularity: SpotKlineInterval;
+  granularity: SpotKlineIntervalV2;
   startTime?: string;
   endTime?: string;
   limit?: string;
